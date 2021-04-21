0b9cc9bb9878f3cee9df3b38dfa667ecb37eb03a
Mark Yasutake <mark.yasutake@gmail.com>
2021-04-21 01:40:19 -0700

    Define BaseElement class
    Define superclass of BasePage and BaseElement

--- page_objects/base.py
+++ page_objects/base.py
@@ -7,11 +7,9 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 
-class BasePage(metaclass=abc.ABCMeta):
+class Base(metaclass=abc.ABCMeta):
 
-    def __init__(self, driver: WebDriver, url: str = None, desc: str = None) -> None:
-        self._driver = driver
-        self._url = url
+    def __init__(self, desc: str = None) -> None:
         self._desc = desc
         self._locators = dict()
         return
@@ -25,13 +23,12 @@ class BasePage(metaclass=abc.ABCMeta):
             return True
 
     @abc.abstractmethod
-    def is_loaded(self) -> bool:
+    def _find_element(self, locator: dict) -> WebElement:
         pass
 
-    def load_page(self) -> None:
-        self._load_page()
-        self.wait_until_loaded()
-        return
+    @abc.abstractmethod
+    def is_loaded(self) -> bool:
+        pass
 
     def wait_until_loaded(self, timeout=5.0, must_load=True) -> None:
         end_time = time.time() + timeout
@@ -46,6 +43,20 @@ class BasePage(metaclass=abc.ABCMeta):
         else:
             logging.warning(log_str)
             return
+
+
+class BasePage(Base, metaclass=abc.ABCMeta):
+
+    def __init__(self, driver: WebDriver, url: str = None, desc: str = None) -> None:
+        super().__init__(desc=desc)
+        self._driver = driver
+        self._url = url
+        return
+
+    def load_page(self) -> None:
+        self._load_page()
+        self.wait_until_loaded()
+        return
 
     # Selenium method wrappers
     #   In the event the Selenium methods get renamed, you only need to change them here.
@@ -72,4 +83,32 @@ class BasePage(metaclass=abc.ABCMeta):
 
     def _send_keystrokes_to_element(self, locator: dict, input_str: str) -> None:
         self._find_element(locator).send_keys(input_str)
+        return
+
+
+class BaseElement(Base, metaclass=abc.ABCMeta):
+
+    def __init__(self, element: WebElement, desc: str = None) -> None:
+        super().__init__(desc=desc)
+        self._element = element
+        return
+
+    # Selenium method wrappers
+    #   Same rules as BaseClass apply.
+
+    def _click(self) -> None:
+        self._element.click()
+        return
+
+    def _is_displayed(self) -> bool:
+        return self._element.is_displayed()
+
+    def _find_element(self, locator: dict) -> WebElement:
+        return self._element.find_element(by=locator['by'], value=locator['value'])
+
+    def _find_elements(self, locator: dict) -> list[WebElement]:
+        return self._element.find_elements(by=locator['by'], value=locator['value'])
+
+    def _send_keystrokes(self, input_str: str) -> None:
+        self._element.send_keys(input_str)
         return
