369f7d042109ed80db9fadc3a4294c57f4feeb73
Mark Yasutake <mark.yasutake@gmail.com>
2021-04-21 17:45:42 -0700

    Define methods to handle Pokedex cookies dialog

--- page_objects/pokedex.py
+++ page_objects/pokedex.py
@@ -9,8 +9,15 @@ class PokedexPage(BasePage):
     def __init__(self, driver: WebDriver) -> None:
         super().__init__(driver=driver, url='https://www.pokemon.com/us/pokedex/', desc='Pokedex Page')
         self._locators['nav_main'] = {'by': By.CSS_SELECTOR, 'value': 'nav.main'}
+        self._locators['accept_cookies_button'] = {'by': By.ID, 'value': 'onetrust-accept-btn-handler'}
+        return
+
+    def cookies_dialog_is_displayed(self) -> bool:
+        return self.element_exists_and_is_displayed(locator=self._locators['accept_cookies_button'])
+
+    def accept_cookies(self) -> None:
+        self._click_element(locator=self._locators['accept_cookies_button'])
         return
 
     def is_loaded(self) -> bool:
-        locator = self._locators['nav_main']
-        return self.element_exists(locator) and self._element_is_displayed(locator)
+        return self.element_exists_and_is_displayed(locator=self._locators['nav_main'])
