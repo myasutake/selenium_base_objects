4298adb00329067d5a1106cfc2c6248eeb0b1a7f
Mark Yasutake <mark.yasutake@gmail.com>
2021-04-26 22:13:04 -0700

    Define command line params

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -5,9 +7,27 @@ from selenium.webdriver.remote.webdriver import WebDriver
 import steps.pokedex
 
 
+def pytest_addoption(parser):
+    parser.addoption('--config', action='store')
+    return
+
+
+@pytest.fixture
+def params(request) -> str:
+    return request.config.getoption('--config').lower()
+
+
 @pytest.fixture(scope='function')
-def driver() -> WebDriver:
-    d = webdriver.Chrome()
+def driver(params) -> WebDriver:
+    if params == 'chrome':
+        d = webdriver.Chrome()
+    elif params == 'firefox':
+        d = webdriver.Firefox()
+    else:
+        log_str = f"Invalid 'config' param value '{params}' given."
+        logging.error(log_str)
+        raise ValueError(log_str)
+
     steps.pokedex.load_page(driver=d)
     yield d
 
