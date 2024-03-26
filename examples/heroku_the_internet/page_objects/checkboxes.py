a4065e7341f33681483a759f1ed1c94df89cc3c2
Mark Yasutake <mark.yasutake@gmail.com>
2024-03-25 18:08:50 -0700

    Heroku_the_Internet - refactor - use common Checkbox class

--- examples/heroku_the_internet/page_objects/checkboxes.py
+++ examples/heroku_the_internet/page_objects/checkboxes.py
@@ -1,11 +1,10 @@
 import logging
-import time
 
 import page_objects.base
+import page_objects.common
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
 
 
 class Page(page_objects.base.BasePage):
@@ -20,44 +19,40 @@ class Page(page_objects.base.BasePage):
     # States
 
     def checkbox_1_is_checked(self) -> bool:
-        element = self.get_checkbox_1_element()
-        checked_attribute = element.get_attribute('checked')
-        return bool(checked_attribute)
+        checkbox = self.get_checkbox_1()
+        return checkbox.checked
 
     def checkbox_2_is_checked(self) -> bool:
-        element = self.get_checkbox_2_element()
-        checked_attribute = element.get_attribute('checked')
-        return bool(checked_attribute)
+        checkbox = self.get_checkbox_2()
+        return checkbox.checked
 
     # Actions
 
     def click_checkbox_1(self) -> None:
-        element = self.get_checkbox_1_element()
+        checkbox = self.get_checkbox_1()
         logging.info('Clicking Checkbox 1...')
-        element.click()
-        time.sleep(0.5)
+        checkbox.click()
         return
 
     def click_checkbox_2(self) -> None:
-        element = self.get_checkbox_2_element()
+        checkbox = self.get_checkbox_2()
         logging.info('Clicking Checkbox 2...')
-        element.click()
-        time.sleep(0.5)
+        checkbox.click()
         return
 
     # Misc
 
     def is_loaded(self) -> bool:
-        checkbox_1_element = self.get_checkbox_1_element()
-        checkbox_2_element = self.get_checkbox_2_element()
-        if not checkbox_1_element.is_displayed():
+        checkbox_1 = self.get_checkbox_1()
+        checkbox_2 = self.get_checkbox_2()
+        if not checkbox_1.element.is_displayed():
             return False
-        return checkbox_2_element.is_displayed()
+        return checkbox_2.element.is_displayed()
 
-    def get_checkbox_1_element(self) -> WebElement:
+    def get_checkbox_1(self) -> page_objects.common.Checkbox:
         elements = self.find_elements(locator=self._locators['checkboxes'], scope='driver')
-        return elements[0]
+        return page_objects.common.Checkbox(element=elements[0], desc='Checkbox 1')
 
-    def get_checkbox_2_element(self) -> WebElement:
+    def get_checkbox_2(self) -> page_objects.common.Checkbox:
         elements = self.find_elements(locator=self._locators['checkboxes'], scope='driver')
-        return elements[1]
+        return page_objects.common.Checkbox(element=elements[1], desc='Checkbox 2')
