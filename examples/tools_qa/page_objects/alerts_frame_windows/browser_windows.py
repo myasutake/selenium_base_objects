254f4cafc66782e401defc1ff3a068fdd6707e27
Mark Yasutake <mark.yasutake@gmail.com>
2024-04-07 17:14:13 -0700

    ToolsQA - Define page objects for all pages except Book Store Application
    Book Store Application requires a little more logic to determine if the page has loaded

---
+++ examples/tools_qa/page_objects/alerts_frame_windows/browser_windows.py
@@ -0,0 +1,19 @@
+import page_objects.base
+import examples.tools_qa.page_objects.common
+
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class Page(examples.tools_qa.page_objects.common.Page):
+
+    def __init__(self, driver: WebDriver) -> None:
+        super().__init__(driver)
+        self._url = 'https://demoqa.com/browser-windows'
+        self._name = 'Alerts_Frame_Windows/Browser-Windows Page'
+        return
+
+    def is_loaded(self) -> bool:
+        title_element = self.find_element(locator=self._locators['title'])
+        if not title_element.is_displayed():
+            return False
+        return title_element.text == 'Browser Windows'
---
+++ examples/tools_qa/page_objects/widgets/date_picker.py
@@ -0,0 +1,19 @@
+import page_objects.base
+import examples.tools_qa.page_objects.common
+
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class Page(examples.tools_qa.page_objects.common.Page):
+
+    def __init__(self, driver: WebDriver) -> None:
+        super().__init__(driver)
+        self._url = 'https://demoqa.com/date-picker'
+        self._name = 'Widgets/Date-Picker Page'
+        return
+
+    def is_loaded(self) -> bool:
+        title_element = self.find_element(locator=self._locators['title'])
+        if not title_element.is_displayed():
+            return False
+        return title_element.text == 'Date Picker'
