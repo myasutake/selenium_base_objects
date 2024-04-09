8831672c69ecfa243edd9718f3a1ef575be3bae9
Mark Yasutake <mark.yasutake@gmail.com>
2024-04-08 19:24:46 -0700

    ToolsQA - refactor Side Nav Group tests into their own section

--- examples/tools_qa/tests/test_side_nav.py
+++ examples/tools_qa/tests/test_side_nav.py
@@ -33,61 +33,64 @@ def navigate_to_page_and_verify_initial_nav_state(navigate_to_page) -> examples.
     return nav
 
 
-def test_clicking_an_expanded_group_closes_it(navigate_to_page_and_verify_initial_nav_state) -> None:
-    nav = navigate_to_page_and_verify_initial_nav_state
+class TestGroups:
 
-    group_name = 'Elements'
-    nav.click_group_header_button(group_name=group_name)
-    assert nav.group_is_collapsed(group_name=group_name)
-    return
+    @staticmethod
+    def test_clicking_an_expanded_group_closes_it(navigate_to_page_and_verify_initial_nav_state) -> None:
+        nav = navigate_to_page_and_verify_initial_nav_state
 
-
-def test_clicking_a_closed_group_expands_it(navigate_to_page_and_verify_initial_nav_state) -> None:
-    nav = navigate_to_page_and_verify_initial_nav_state
-
-    group_name = 'Forms'
-    nav.click_group_header_button(group_name=group_name)
-    assert nav.group_is_expanded(group_name=group_name)
-    return
-
-
-def test_clicking_multiple_times(navigate_to_page_and_verify_initial_nav_state) -> None:
-    nav = navigate_to_page_and_verify_initial_nav_state
-
-    group_name = 'Elements'
-    number_of_loops = 10
-
-    for _ in range(number_of_loops):
+        group_name = 'Elements'
         nav.click_group_header_button(group_name=group_name)
         assert nav.group_is_collapsed(group_name=group_name)
+        return
+
+    @staticmethod
+    def test_clicking_a_closed_group_expands_it(navigate_to_page_and_verify_initial_nav_state) -> None:
+        nav = navigate_to_page_and_verify_initial_nav_state
+
+        group_name = 'Forms'
         nav.click_group_header_button(group_name=group_name)
         assert nav.group_is_expanded(group_name=group_name)
+        return
 
-    return
+    @staticmethod
+    def test_clicking_multiple_times(navigate_to_page_and_verify_initial_nav_state) -> None:
+        nav = navigate_to_page_and_verify_initial_nav_state
 
+        group_name = 'Elements'
+        number_of_loops = 10
 
-def test_max_one_expanded_group(navigate_to_page_and_verify_initial_nav_state) -> None:
-    nav = navigate_to_page_and_verify_initial_nav_state
+        for _ in range(number_of_loops):
+            nav.click_group_header_button(group_name=group_name)
+            assert nav.group_is_collapsed(group_name=group_name)
+            nav.click_group_header_button(group_name=group_name)
+            assert nav.group_is_expanded(group_name=group_name)
 
-    click_sequence = ['Forms',
-                      'Alerts, Frame & Windows',
-                      'Widgets',
-                      'Interactions',
-                      'Book Store Application',
-                      'Elements',
-                      'Widgets',
-                      'Interactions',
-                      'Forms',
-                      'Alerts, Frame & Windows']
-    for i_group_name in click_sequence:
-        nav.click_group_header_button(group_name=i_group_name)
-        assert len(nav.expanded_groups) == 1
+        return
 
-    return
+    @staticmethod
+    def test_max_one_expanded_group(navigate_to_page_and_verify_initial_nav_state) -> None:
+        nav = navigate_to_page_and_verify_initial_nav_state
 
+        click_sequence = ['Forms',
+                          'Alerts, Frame & Windows',
+                          'Widgets',
+                          'Interactions',
+                          'Book Store Application',
+                          'Elements',
+                          'Widgets',
+                          'Interactions',
+                          'Forms',
+                          'Alerts, Frame & Windows']
+        for i_group_name in click_sequence:
+            nav.click_group_header_button(group_name=i_group_name)
+            assert len(nav.expanded_groups) == 1
 
-def test_no_expanded_groups_is_possible(navigate_to_page_and_verify_initial_nav_state) -> None:
-    nav = navigate_to_page_and_verify_initial_nav_state
-    nav.click_group_header_button(group_name='Elements')
-    assert len(nav.expanded_groups) == 0
-    return
+        return
+
+    @staticmethod
+    def test_no_expanded_groups_is_possible(navigate_to_page_and_verify_initial_nav_state) -> None:
+        nav = navigate_to_page_and_verify_initial_nav_state
+        nav.click_group_header_button(group_name='Elements')
+        assert len(nav.expanded_groups) == 0
+        return
